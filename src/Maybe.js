ac8cc714747d7005fbdcf4f6c4c5c1336960dbd6
mwardle <mwardle@robinsage.com>
2016-01-04 19:01:00 -0700

    Implemented maybe

---
+++ src/Maybe.js
@@ -0,0 +1,84 @@
+var _ = require('lambdash');
+
+var Maybe = function Maybe(value) {
+    if (value == null) {
+        return Maybe.Nothing;
+    }
+    return Maybe.Just(value);
+};
+
+Maybe = _.Type.sum(Maybe, {Just: {value: null}, Nothing: []});
+
+Maybe.isJust = Maybe.case({
+    Just: true,
+    Nothing: false
+});
+
+Maybe.isNothing = Maybe.case({
+    Just: false,
+    Nothing: true
+});
+
+Maybe.compare = _.curry(function(left, right) {
+    if (Maybe.isNothing(left)) {
+        if (Maybe.isNothing(right)) {
+            return _.EQ;
+        }
+
+        return _.LT;
+    }
+
+    if (Maybe.isNothing(right)) {
+        return _.GT;
+    }
+
+    return _.compare(left.value, right.value);
+});
+
+Maybe.map = _.curry(function(fn, maybe) {
+    return Maybe.case({
+        Nothing: Maybe.Nothing,
+        Just: function(value) {
+            return Maybe(fn(value));
+        }
+    }, maybe);
+});
+
+Maybe.concat = _.curry(function(left, right) {
+    if (Maybe.isNothing(left) || Maybe.isNothing(right)) {
+        return Maybe.Nothing;
+    }
+
+    return Maybe(_.concat(left.value, right.value));
+});
+
+Maybe.empty = _.always(Maybe.Nothing);
+Maybe.of = Maybe;
+
+Maybe.foldl = _.curry(function(fn, accum, maybe) {
+    return Maybe.case({
+        "Nothing": accum,
+        "Just": function(value) {
+            return fn(accum, value);
+        }
+    }, maybe)
+});
+
+Maybe.foldr = Maybe.foldl;
+Maybe.fold = Maybe.foldl;
+
+Maybe.ap = _.curry(function(apply, functor){
+    return Maybe.isNothing(apply) ? Maybe.Nothing : Maybe.map(apply.value, functor);
+});
+
+Maybe.flatten = Maybe.case({
+    "Nothing": Maybe.Nothing,
+    "Just": _.identity
+});
+
+Maybe.show = Maybe.case({
+    "Nothing": _.always("Maybe.Nothing"),
+    "Just": function(value) {
+        return "Maybe.Just(" + _.show(value) + ")";
+    }
+});
